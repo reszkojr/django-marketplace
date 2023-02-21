5c5fab6662fca776e9640c689ad48e1356fcee21
reszkojr <s.reszkojr@gmail.com>
2023-02-21 17:48:54 -0300

    Contact view and Welcome page

--- django_marketplace/urls.py
+++ django_marketplace/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from core.views import index
+from core.views import *
 
 urlpatterns = [
     path('', index, name='index'),
+    path('contact/', contact, name='contact'),
     path('admin/', admin.site.urls),
 ]
