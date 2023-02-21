def95bef8307887559f3763b8adfc2a19b7df3f3
reszkojr <s.reszkojr@gmail.com>
2023-02-21 18:20:56 -0300

    Item model & some Category model adjustments

---
+++ item/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        ordering = ('name',)
+        verbose_name_plural = 'Categories'
+
+class Item(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank=True, null=True)
+    price = models.FloatField()
+    is_sold = models.BooleanField(default=False)
+    image = models.ImageField(upload_to='item_images', blank=True, null=True)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    category = models.ForeignKey(Category, related_name='items', on_delete=models.CASCADE)
+    created_by = models.ForeignKey(User, related_name='items', on_delete=models.CASCADE)
