626559b8df0abbbd50df02eb82d8e11dec5f8c58
reszkojr <s.reszkojr@gmail.com>
2023-02-22 16:44:33 -0300

    Hiding the secret key

--- django_marketplace/settings.py
+++ django_marketplace/settings.py
@@ -1,3 +1,5 @@
+from decouple import config
+
 """
 Django settings for django_marketplace project.
 
@@ -20,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-^5_w3eaw6jvb0(%_pl8!%btx^vm6qwpzn^jnd0nfoinsq#kr6p'
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
